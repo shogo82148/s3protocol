482f4142ad4d3984e5fbee1435a1ca7ec9965766
Ichinose Shogo <shogo82148@gmail.com>
2020-04-12 18:36:12 +0900

    add test cases for headers

--- transport_test.go
+++ transport_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
@@ -30,19 +31,39 @@ func (mock *s3mock) HeadObjectWithContext(ctx context.Context, in *s3.HeadObject
 func TestRoundTrip(t *testing.T) {
 	mock := &s3mock{
 		getObjectWithContext: func(ctx context.Context, in *s3.GetObjectInput, _ ...request.Option) (*s3.GetObjectOutput, error) {
+			// a test case for string value
 			if aws.StringValue(in.VersionId) != "foobar" {
 				t.Errorf("unexpected version id: want %q, got %q", "footbar", aws.StringValue(in.VersionId))
 			}
+
+			// a test case for time.Time
+			if want, got := time.Date(2015, time.October, 21, 7, 28, 0, 0, time.UTC), in.IfModifiedSince; got == nil || !got.Equal(want) {
+				t.Errorf("unexpected If-Modified-Since: want %s, got %s", want, got)
+			}
+
+			// a test case for integer value
+			if aws.Int64Value(in.PartNumber) != 1 {
+				t.Errorf("unexpected part number: want %d, got %d", 1, aws.Int64Value(in.PartNumber))
+			}
+
 			return &s3.GetObjectOutput{
-				ContentType: aws.String("plain/text"),
-				Body:        ioutil.NopCloser(strings.NewReader("Hello S3!")),
+				ContentType:  aws.String("plain/text"),
+				DeleteMarker: aws.Bool(false),
+				LastModified: aws.Time(time.Date(2015, time.October, 21, 7, 28, 0, 0, time.UTC)),
+				PartsCount:   aws.Int64(10),
+				Body:         ioutil.NopCloser(strings.NewReader("Hello S3!")),
 			}, nil
 		},
 	}
 	tr := &http.Transport{}
 	tr.RegisterProtocol("s3", &Transport{S3: mock})
 	c := &http.Client{Transport: tr}
-	resp, err := c.Get("s3://bucket-name/object-key?versionId=foobar")
+	req, err := http.NewRequest(http.MethodGet, "s3://bucket-name/object-key?versionId=foobar&partNumber=1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("If-Modified-Since", "Wed, 21 Oct 2015 07:28:00 GMT")
+	resp, err := c.Do(req)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -58,8 +79,25 @@ func TestRoundTrip(t *testing.T) {
 	if string(got) != "Hello S3!" {
 		t.Errorf("want Hello S3!, got %s", string(got))
 	}
+
+	// a test case for string value
 	if resp.Header.Get("Content-Type") != "plain/text" {
 		t.Errorf("want %s, got %s", "plain/text", resp.Header.Get("Content-Type"))
+	}
+
+	// a test case for boolean value
+	if resp.Header.Get("X-Amz-Delete-Marker") != "false" {
+		t.Errorf("want %s, got %s", "false", resp.Header.Get("X-Amz-Delete-Marker"))
+	}
+
+	// a test case for time.Time
+	if resp.Header.Get("Last-Modified") != "Wed, 21 Oct 2015 07:28:00 GMT" {
+		t.Errorf("want %s, got %s", "Wed, 21 Oct 2015 07:28:00 GMT", resp.Header.Get("Last-Modified"))
+	}
+
+	// a test case for integer value
+	if resp.Header.Get("X-Amz-Mp-Parts-Count") != "10" {
+		t.Errorf("want %s, got %s", "10", resp.Header.Get("X-Amz-Mp-Parts-Count"))
 	}
 }
 
